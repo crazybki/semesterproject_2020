36048ef2251820232f52fbbddac493556588bf84
bki123 <41273107+crazybki@users.noreply.github.com>
2020-12-15 22:50:01 +0100

    new functionality in js

--- JS/frontpage.js
+++ JS/frontpage.js
@@ -1,8 +1,14 @@
 import { getCharacters } from "/JS/got_JSON.js";
 
-
+let storeImg = [];
 const getCharaterFaces = document.querySelector('.container_characters');
-const face = document.querySelector('.chosenFace');
+const face = document.querySelector('#chosenFace');
+const opponents = document.querySelector('#opponent')
+let characterChosen = false;
+let starting = false;
+
+
+
 
 getCharacters.forEach(element => {
     getCharaterFaces.innerHTML += `<div>
@@ -20,27 +26,50 @@ images.forEach(buttons => {
     buttons.addEventListener('click', characterIsChosen)
 });
 
+//
 
-function characterIsChosen() {
-    const img = this.src;
-    const imgUrl = img.slice(21,60);
-    let name = this.dataset.name;    
-
-    let arr = [name, imgUrl];
-    console.log(arr)
-
-
-    
-    let storeCharacther = localStorage.setItem('url', JSON.stringify(arr));
-    face.innerHTML = `<p>You chose ${name}</p>`;
 
 
 
-    setTimeout(() => {
+function characterIsChosen() {
+    if (!starting) {
+        let img = this.src;
+        let imgUrl = img.slice(21, 60);
+        let name = this.dataset.name;
 
-        if(storeCharacther !== null) {
-            location.href = "/html/games.html";
+        let arr = { name: name, image: imgUrl }
+
+        storeImg.push(arr)
+
+        localStorage.setItem('character', JSON.stringify(storeImg));
+
+        if (!characterChosen) {
+            seeChosenCharacter(img, name);
+            characterChosen = true;
+        } else {
+            starting = true;
+            displayOpponent(img, name);
+            setTimeout(() => {
+                window.location.href = "/html/games.html";
+                console.log('redirect')
+            }, 1000);
         }
-    }, 4000);
+    }
+};
+
+
+function seeChosenCharacter(image, nameOfcharacter) {
+    face.innerHTML = `<div id="test12">
+                                    <img src="${image}">
+                                    <p>You chose ${nameOfcharacter}</p>
+                          </div>`;
+};
+
+function displayOpponent(image, nameOfcharacter) {
+    opponents.innerHTML = `<div id="test12">
+                                    <img src="${image}">
+                                    <p>You chose ${nameOfcharacter}</p>
+                          </div>`;
+
 
 };
