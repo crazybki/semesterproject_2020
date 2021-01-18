645f4483d61f7d17755f5a460caa37d3e9aad725
bki123 <41273107+crazybki@users.noreply.github.com>
2021-01-18 23:25:30 +0100

    css added on game site, added new js

--- JS/frontpage.js
+++ JS/frontpage.js
@@ -7,6 +7,7 @@ const opponents = document.querySelector('#opponent');
 const counterElement = document.querySelector('.chosenCharaters-countdown');
 const introElement = document.querySelector('.introtxt');
 const chooseCharacterElement = document.querySelector('.pageintro-txt');
+const info = document.querySelector('.chosencharacters__info')
 
 //
 let storeImg = [];
@@ -22,10 +23,10 @@ function infoAboutGame() {
     introElement.innerHTML = `<h2 class="introtxt__head2">Winter is coming!</h2> 
                               <p class="introtxt__message">Your mission is to take the iron throne
                               in the Red Keep. You must arrive before your opponent to the 
-                              throne.</p> 
-                              <p class="introtxt__message">The road is dangerous and treacherous, so be carefull, and 
-                              look out for traps.</p>
-                              <p class="introtxt__charactherchosen">Choose your charachter</p>`
+                              throne. The road is dangerous and treacherous, so be carefull, and 
+                              look out for traps.</p> 
+                              
+                              <p class="introtxt__charactherchosen">Choose your charachters</p>`
 }
 
 infoAboutGame();
@@ -94,15 +95,17 @@ function characterIsChosen() {
 function player1Chosen(image, nameOfcharacter) {
     face.innerHTML = `<div class="chosencharacters_player1">
                                     <img class="player1" src="${image}">
-                                    <p>${nameOfcharacter}</p>
+                                    <p class="names">${nameOfcharacter}</p>
                           </div>`;
+    info.innerHTML = `<div class="chosencharacter_opponent">Choose your opponents</div>`
 };
 
 function player2Chosen(image, nameOfcharacter) {
     opponents.innerHTML = `<div class="chosencharacters_player2">
                                     <img class="player2" src="${image}">
-                                    <p>${nameOfcharacter}</p>
+                                    <p class="names">${nameOfcharacter}</p>
                           </div>`;
+    info.innerHTML = '';
 };
 
 
@@ -114,7 +117,7 @@ function gameStarting() {
 
         } else {
             counter--;
-            counterElement.innerHTML = `<span class="counter"> Game begins in ${counter}</span>`;
+            counterElement.innerHTML = `<span class="counter">Game begins in ${counter}</span>`;
         }
 
     }, 1000);
--- JS/games.js
+++ JS/games.js
@@ -3,7 +3,7 @@ import { getCharacters } from "/JS/got_JSON.js";
 const listOfCharacters = getCharacters;
 
 //Storing the images chosen from user
-const playerImg = document.querySelector('.boardcontainer');
+const playerImg = document.querySelector('.boardcontainer__playerimg');
 
 
 //Get button, for dices and for players
@@ -12,13 +12,14 @@ const diceBtnMachine = document.querySelector('#btn_dice2');
 const diceEl = document.querySelector('#dice1');
 const diceEl2 = document.querySelector('#dice2');
 const player1 = document.querySelector('#player1');
-
+const dice6P1 = document.querySelector('.bordercontainer__rolledsixhuman')
+const dice6P2 = document.querySelector('.bordercontainer__rolledsixmachine')
 const movepawn = document.querySelector('.player1pawn');
 const movepawn1 = document.querySelector('.player2pawn');
 
 const player2 = document.querySelector('#player2');
 let trapMessage = document.querySelector('.message');
-diceBtnMachine.disabled = true;
+//diceBtnMachine.disabled = true;
 
 //Sounds
 const rolledSixSound = new Audio('/sounds/playerRolled6.wav')
@@ -36,18 +37,39 @@ const gridTile = ['#grid_6', '#grid_10', '#grid_15', '#grid_20', '#grid_29'];
 
 //
 window.addEventListener('load', (event) => {
-    async function loader() {
-        try {
-            // gotTheme.play();
-            diceEl.innerHTML = `<img src="/images/1x/dice1_1.png">`;
-            diceEl2.innerHTML = `<img src="/images/1x/dice2_1.png">`;
-        }
-        catch (error) {
-            console.log('Error has accoured')
-        }
-    }
-    loader()
+    gotTheme.play();
+    diceEl.innerHTML = `<img class="dice__player1" src="/images/1x/dice1_1.png">`;
+    // diceEl2.innerHTML = `<img class="dice__player2" src="/images/1x/dice2_1.png">`;
+
 });
+
+
+//New testing
+
+let playersTurn = false;
+let machineTurn = false;
+
+diceEl.addEventListener('click', rolleDiceNumber);
+
+function rolleDiceNumber() {
+    let diceRoll = Math.floor(Math.random() * 6) + 1;
+    console.log(diceRoll)
+    diceEl.innerHTML = `<img class="dice__player1" src="/images/1x/dice1_${diceRoll}.png">`;
+
+    if (!playersTurn) {
+        playersTurn = true;
+        movePlayer1(diceRoll);
+        player1Rolled6(diceRoll);
+    } else {
+        machineTurn = true;
+        playersTurn = false;
+        movePlayer2(diceRoll);
+        machineRolled6(diceRoll)
+    }
+};
+
+//New testing
+
 
 
 /**
@@ -61,14 +83,14 @@ function characterRetrived() {
     let infoAboutFaces = JSON.parse(faceRetrieved);
 
     playerImg.innerHTML += `<div class="boardcontainer__player1">
-                                <img src="${infoAboutFaces[0].image}">
+                                <img class="boardcontainer__imgplayer1" src="${infoAboutFaces[0].image}">
                                 <p>${infoAboutFaces[0].name}</p>
                             </div>
                             <div class="boardcontainer__player2">
-                                <img src="${infoAboutFaces[1].image}">
+                                <img class="boardcontainer__imgplayer1" src="${infoAboutFaces[1].image}">
                                 <p>${infoAboutFaces[1].name}</p>
                             </div>`;
-
+    console.log(infoAboutFaces)
     player1.innerHTML = `<img class="player1pawn" src="${infoAboutFaces[0].gamePawn}">`;
     player2.innerHTML = `<img class="player2pawn" src="${infoAboutFaces[1].gamePawn}">`;
 };
@@ -78,14 +100,16 @@ characterRetrived();
 
 
 //Event listener for rolling the dice
-diceBtnPlayer.addEventListener('click', rollDice);
+
+
+/**diceBtnPlayer.addEventListener('click', rollDice);
 diceBtnMachine.addEventListener('click', rollDiceMachine);
 
 function rollDice(e) {
 
     e.preventDefault();
     let diceRoll = Math.floor(Math.random() * 6) + 1;
-    diceEl.innerHTML = `<img src="/images/1x/dice1_${diceRoll}.png">`;
+    diceEl.innerHTML = `<img class="dice__player1" src="/images/1x/dice1_${diceRoll}.png">`;
 
     if (rollDice) {
 
@@ -103,7 +127,7 @@ function rollDiceMachine(e) {
 
     e.preventDefault();
     let diceRoll2 = Math.floor(Math.random() * 6) + 1;
-    diceEl2.innerHTML = `<img src="/images/1x/dice2_${diceRoll2}.png">`;
+    diceEl2.innerHTML = `<img class="dice__player2" src="/images/1x/dice2_${diceRoll2}.png">`;
 
     if (rollDiceMachine) {
         diceBtnMachine.disabled = true;
@@ -114,7 +138,7 @@ function rollDiceMachine(e) {
     movePlayer2(diceRoll2);
     machineRolled6(diceRoll2)
 };
-
+**/
 
 //Function to move the player, gets the diceroll from function rollDice
 function movePlayer1(diceRoll) {
@@ -124,9 +148,8 @@ function movePlayer1(diceRoll) {
 
     if (player1Postion >= 32) {
         document.querySelector('#grid_32').appendChild(player1);
-        //window.location.href = "/html/winnerpage.html"
-        console.log('Player 1 is the winner')
-        playerWinsGame.play();
+        window.location.href = "/html/winnerpage.html"
+        //playerWinsGame.play();
     } else {
         document.querySelector(newGridId).appendChild(player1);
     }
@@ -144,9 +167,8 @@ function movePlayer2(diceRoll2) {
 
     if (player2Postion >= 32) {
         document.querySelector('#grid_32').appendChild(player2);
-        // window.location.href = "/html/winnerpage.html"
-        console.log('player 2 is the winner');
-        playerWinsGame.play();
+        window.location.href = "/html/winnerpage.html"
+        //playerWinsGame.play();
     } else {
         document.querySelector(newGridId).appendChild(player2);
     }
@@ -173,7 +195,7 @@ function checkForTrapsP1(updatedGridId, pos) {
 
 
     }
-}
+};
 
 function checkForTrapsP2(updateTile) {
     let trap = gridTile.includes(updateTile);
@@ -185,35 +207,48 @@ function checkForTrapsP2(updateTile) {
             player2Postion = 1
             originalState.appendChild(player2)
         }, 1800);
-
     }
-}
+};
 
 
 function trap1() {
-    trapMessage.innerHTML = `<p class="message-txt">Ooh no, you stepped on a 
-                             trap, move back to square one</p>`;
+    trapMessage.innerHTML = `<p class="message-txt">You stepped on a 
+                             trap. Move back to square one</p>`;
 
     setTimeout(() => {
         trapMessage.innerHTML = '';
-    }, 4000);
-}
+    }, 1800);
+};
+
 
 
 
 // User has rolled 6 and gets a new chance to roll the dice
 function player1Rolled6(rollingDice) {
     if (rollingDice === 6) {
-        diceBtnPlayer.disabled = false;
-        diceBtnMachine.disabled = true;
-        rolledSixSound.play();
+        // diceBtnPlayer.disabled = false;
+        // diceBtnMachine.disabled = true;
+        //rolledSixSound.play();
+        dice6P1.innerHTML = `<p class="message-txt">You rolled a six, throw the dice again`
+
+        setTimeout(() => {
+            dice6P1.innerHTML = '';
+        }, 3000);
     }
-}
+};
+
 
 function machineRolled6(machineRoll) {
     if (machineRoll === 6) {
-        diceBtnPlayer.disabled = true;
-        diceBtnMachine.disabled = false;
-        rolledSixSound.play();
+        // diceBtnPlayer.disabled = true;
+        // diceBtnMachine.disabled = false;
+        // rolledSixSound.play();
+
+        dice6P2.innerHTML = `<p class="message-txt">You rolled a six, throw the dice again`
+
+        setTimeout(() => {
+            dice6P2.innerHTML = '';
+        }, 3000);
     }
-}
+};
+
